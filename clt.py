8ab79c4b2b850dcdc59e3cd89dcefcc869f79e32
Ryan Lafferty <laffertyryan0@gmail.com>
2019-09-01 12:27:27 -0400

    added more distribution options

--- clt.py
+++ clt.py
@@ -6,30 +6,40 @@ def get_sample(sample_size, dist = 'uniform'):
     sample = []
     if(dist == 'uniform'):
         sample = [random.uniform(0,1) for i in range(sample_size)]
+    elif(dist == 'exponential'):
+        sample = [random.expovariate(1) for i in range(sample_size)]
+    elif(dist == 'triangular'):
+        sample = [random.triangular(0,1,.5) for i in range(sample_size)]
     else:
         raise(Exception('Distribution not found.'))
     return sample
 
-def get_sample_dist(sample_size = 30, trials = 10000, dist = 'uniform'):
+def get_sample_dist(sample_size = 30,
+                    trials = 10000,
+                    dist = 'uniform',
+                    x_min = 0,
+                    x_max = 1,
+                    dx = .01):
     samples = [get_sample(sample_size,dist) for i in range(trials)]
 
     #compute sample means
     means = [sum(sample)/len(sample) for sample in samples]
 
-    #compute pdf using the following approximation
-    #p(x) dx = Pr(x<mean<x+dx)
-
-    x_min = 0
-    x_max = 1
-    dx = .001
+    x_min = x_min
+    x_max = x_max
+    dx = dx
 
     xvals = []
     pdf = []
 
+
+    #compute pdf using the following approximation
+    #p(x) dx = Pr(x-dx/2<mean<x+dx/2)
+
     for i in range(round((x_max - x_min)/dx)):
-        x = -x_min + i * dx
+        x = x_min + i * dx
         xvals.append(x)
-        filt = lambda y: x < y < x + dx
+        filt = lambda y: x-dx/2 < y < x + dx/2
         prob = len(list(filter(filt,means)))/trials
         pdf.append(prob/dx)
 
@@ -37,14 +47,19 @@ def get_sample_dist(sample_size = 30, trials = 10000, dist = 'uniform'):
 
 
 #plot the sample distribution
-def plot_dist(sample_size = 30, trials = 10000, dist = 'uniform'):
-    xvals, pdf = get_sample_dist(sample_size,trials,dist)
+def plot_dist(sample_size = 30,
+                    trials = 10000,
+                    dist = 'uniform',
+                    x_min = 0,
+                    x_max = 1,
+                    dx = .01):
+    xvals, pdf = get_sample_dist(sample_size,trials,dist,x_min,x_max)
     #display the distribution of sample mean
     plt.plot(xvals,pdf)
     plt.show()
         
 def main():
-    plot_dist(50,10000)
+    plot_dist(100,10000,dist = 'triangular')
 
 if __name__ == '__main__':
     main()
